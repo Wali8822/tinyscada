36b504116b6686b1b156c9bcb359c2e287a72f81
蒋玉柱 <jiangyuzhu8822@163.com>
2017-07-09 16:02:42 +0800

    增加服务端功能

---
+++ rdb/src/server.h
@@ -0,0 +1,18 @@
+
+
+
+#ifndef __SERVER_H__
+#define __SERVER_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+
+int init_server();
+
+#ifdef __cplusplus
+}
+#endif // __cplusplus
+
+#endif
